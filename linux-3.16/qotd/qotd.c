145315cdf532c45b6aa753d98260d2b1c0b63abc
root <jj1232727>
2019-10-12 04:12:53 -0700

    Finally Finally

--- linux-3.16/qotd/qotd.c
+++ linux-3.16/qotd/qotd.c
@@ -1,12 +1,20 @@
 #include <linux/kernel.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <linux/random.h>
+
 
 asmlinkage long sys_qotd(void)
 {
 	const char* quotes[] = {"All that we are is the result of what we have thought", "I have no special talent. I am only passionately curious.", "If you judge people, you have no time to love them.", "Wisely, and slow. They stumble that run fast."};
 	const size_t cquotes = sizeof(quotes)/sizeof(quotes[0]);
-        printk("%s\n", quotes[rand() % cquotes]);
+	
+	int rChoice;
+	get_random_bytes(&rChoice, sizeof(rChoice));
+
+	if(rChoice < 0){
+		rChoice = -rChoice;	
+	}
+
+        printk("%s\n", quotes[rChoice % cquotes]);
         return 0;
 }
 
